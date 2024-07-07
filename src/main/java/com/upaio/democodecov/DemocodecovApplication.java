abe1410d3e1c968c4807aa40cbd28f9f0e4e617c
aguirre-jes <66501518+aguirre-jes@users.noreply.github.com>
2024-07-07 18:00:48 -0500

    init: base repo

---
+++ src/main/java/com/upaio/democodecov/DemocodecovApplication.java
@@ -0,0 +1,13 @@
+package com.upaio.democodecov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocodecovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocodecovApplication.class, args);
+	}
+
+}
